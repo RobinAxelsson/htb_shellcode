1adac73f579351152083e36dacac2cf2dc8696e1
RobinSalihAxelsson <axelsson.robin@outlook.com>
2024-12-16 13:42:56 +0100

    faeet: add loader, assembler

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 alias assf="./scripts/assf.sh"
 alias shellcoder="python3 ./scripts/shellcoder.py"
+alias loader="python3 ./scripts/loader.py"
+alias assembler="python3 ./scripts/assembler.py"
